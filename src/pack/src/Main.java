8afa42f797c6f5bac3f701016e6482c76672d2d9
Andrea <andrea.vergine01@universitadipavia.it>
2020-07-29 18:27:27 +0200

    new classes

--- src/pack/src/Main.java
+++ src/pack/src/Main.java
@@ -1,6 +1,12 @@
 package src;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -8,10 +14,39 @@ public class Main {
 			System.err.println("Use this format: java Main name_img");
 			System.exit(1);
 		}
-		java.awt.image.BufferedImage img = Image.read(args[0]);
+		//java.awt.image.BufferedImage img = Image.read(args[0]);
+		File f = new File("Immagini/tramonto-mareImg.jpg");
+		java.awt.image.BufferedImage img = ImageIO.read(f);
+		
 		int w = img.getWidth();
 		int h = img.getHeight();
+		int imgSize = w * h;
+		
+		Scanner input = new Scanner (System.in);
+		String text = input.nextLine();
+		input.close();
+		
+		int lenght = text.length();
+		if(lenght > (h-1)) {
+			System.err.println("Message too long for this picture");
+			System.exit(1);
+		}
+		
+		Row[] rows = new Row[h];
+		Thread[] threads = new Thread[h];
+		
+		for(int i=0; i<lenght; i++) {
+			int[] rgb = img.getRGB(0, i, w, 1, null, 0, w);
+			rows[i] = new Row(rgb, text.charAt(i));
+			threads[i] = new Thread(rows[i]);
+		}
 		
 		
+		
+		
+		
+        File g = new File("Immagini/tramonto-mareProva.jpg");
+        ImageIO.write(img, "jpg", g);
+        BufferedImage img2 = ImageIO.read(g);
 	}
 }
--- src/pack/src/Row.java
+++ src/pack/src/Row.java
@@ -1,5 +1,25 @@
 package src;
 
-public class Row {
-
+public class Row implements Runnable {
+	private int[] rgbArray;
+	private char s;
+	private boolean done = false;
+	private boolean integer = false;
+	
+	public Row(int[] a, char s) {
+		rgbArray = a;
+		this.s=s;
+	}
+	
+	public Row(int[] a, int size) {
+		this.integer = true;
+		//if integer==true ...
+		//chiamare embedInteger del nostro amico
+	}
+	
+	public void run() {
+		//if integer==false ...
+		//chiamare embedByte del nostro amico
+		done= true;
+	}
 }

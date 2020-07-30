c20d70f06a8f3b3ecd61c3712d298a7fae2e1c3b
angelica seiberl <angiiseiberl@gmail.com>
2020-07-30 17:32:32 +0200

    aggiunto embedInteger in row

--- src/pack/src/Row.java
+++ src/pack/src/Row.java
@@ -3,6 +3,7 @@ package src;
 public class Row implements Runnable {
 	private int[] rgbArray;
 	private char s;
+	private int size;
 	private boolean done = false;
 	private boolean integer = false;
 	
@@ -13,13 +14,46 @@ public class Row implements Runnable {
 	
 	public Row(int[] a, int size) {
 		this.integer = true;
+		this.size = size;
 		//if integer==true ...
 		//chiamare embedInteger del nostro amico
 	}
 	
 	public void run() {
+		if(integer) {
+			embedInteger(rgbArray, size, 0);
+		}
+		else {
+			
+		}
 		//if integer==false ...
 		//chiamare embedByte del nostro amico
 		done= true;
+	}
+	
+	private void embedInteger(int[] rgb, int n, int storageBit) {
+		for (int i = 0; i < rgb.length; i++) {
+
+			int bit = getBitValue(n, i);
+			rgb[i] = setBitValue(rgb[i], storageBit, bit);
+			i++;
+
+		}
+	}
+
+	private int getBitValue(int n, int location) {
+		int v = n & (int) Math.round(Math.pow(2, location));
+		return v == 0 ? 0 : 1;
+	}
+
+	private int setBitValue(int n, int location, int bit) {
+		int toggle = (int) Math.pow(2, location), bv = getBitValue(n, location);
+		if (bv == bit)
+			return n;
+		if (bv == 0 && bit == 1)
+			n |= toggle;
+		else if (bv == 1 && bit == 0)
+			n ^= toggle;
+		return n;
 	}
 }

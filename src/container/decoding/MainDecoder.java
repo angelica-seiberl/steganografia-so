7e5b78547975330b86665a25277a0fcd3e988007
Andrea <andrea.vergine01@universitadipavia.it>
2020-08-14 15:06:50 +0200

    modifiche

--- src/container/decoding/MainDecoder.java
+++ src/container/decoding/MainDecoder.java
@@ -11,12 +11,10 @@ public class MainDecoder {
 			System.err.println("Use this format: java Main name_img");
 			System.exit(1);
 		}
-		//java.awt.image.BufferedImage img = Image.read(args[0]);
+		
 		File f = new File(args[0]);
 		java.awt.image.BufferedImage img2 = ImageIO.read(f);
 		
-		//File g = new File("Immagini/tramonto-mareProva.png");
-		//BufferedImage img2 = ImageIO.read(g);
 		Decoder decoder = new Decoder();
 		decoder.decode(img2);
 		
